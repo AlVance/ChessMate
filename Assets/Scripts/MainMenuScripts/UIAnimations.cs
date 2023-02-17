8552288ea48b0792d8e73b3bc54823c077b8c9c6
VictorCastellote <victor.castellote@grupoadd.es>
2023-02-17 20:44:51 +0100

    Animacion guaparda fluidas con delay Gallery

--- Assets/Scripts/MainMenuScripts/UIAnimations.cs
+++ Assets/Scripts/MainMenuScripts/UIAnimations.cs
@@ -9,7 +9,9 @@ public class UIAnimations : MonoBehaviour
 {
     public RectTransform levelBut, editorBut;
     public RectTransform editorBG, levelBG;
+    public RectTransform editorCont, levelCont;
     public float time = 0.2f;
+    public float delay = .2f;
 
     private void Start()
     {
@@ -21,23 +23,33 @@ public class UIAnimations : MonoBehaviour
 
         if (isEditor)
         {
-            editorBut.DOAnchorMin(new Vector2(0.4f, 0.0f), time).SetEase(Ease.InOutBack);
-            levelBut.DOAnchorMax(new Vector2(0.4f, 1.0f), time).SetEase(Ease.InOutBack);
+            editorBut.DOAnchorMin(new Vector2(0.4f, 0.0f), time).SetEase(Ease.OutBack);
+            levelBut.DOAnchorMax(new Vector2(0.4f, 1.0f), time).SetEase(Ease.OutBack);
 
-            
-            sequence.Append(levelBG.DOLocalMoveX(-levelBG.rect.width, time*0.5f));
-            sequence.Append(editorBG.DOLocalMoveX(0, time*0.5f));
+            //yield return new WaitForSeconds(.2f);
 
+            editorBG.DOLocalMoveX(0, time * (1 + delay));
+            levelBG.DOLocalMoveX(-levelBG.rect.width, time * (1 + delay));
+
+            //yield return new WaitForSeconds(.2f);
+
+            editorCont.DOLocalMoveX(0, time * (1 + delay * 2));
+            levelCont.DOLocalMoveX(-levelBG.rect.width, time * (1 + delay * 2));
         }
         else
         {
-            editorBut.DOAnchorMin(new Vector2(0.6f, 0.0f), time).SetEase(Ease.InOutBack);
-            levelBut.DOAnchorMax(new Vector2(0.6f, 1.0f), time).SetEase(Ease.InOutBack);
+            editorBut.DOAnchorMin(new Vector2(0.6f, 0.0f), time).SetEase(Ease.OutBack);
+            levelBut.DOAnchorMax(new Vector2(0.6f, 1.0f), time).SetEase(Ease.OutBack);
 
-            
-            sequence.Append(editorBG.DOLocalMoveX(editorBG.rect.width, time*0.5f));
-            sequence.Append(levelBG.DOLocalMoveX(0, time*0.5f));
+            //yield return new WaitForSeconds(.2f);
 
+            editorBG.DOLocalMoveX(editorBG.rect.width, time * (1 + delay));
+            levelBG.DOLocalMoveX(0, time * (1 + delay));
+
+            //yield return new WaitForSeconds(.2f);
+
+            editorCont.DOLocalMoveX(editorBG.rect.width, time * (1 + delay * 2));
+            levelCont.DOLocalMoveX(0, time * (1 + delay * 2));
         }
 
     }
